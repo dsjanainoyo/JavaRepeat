b8fbab03269a4c73b1cd14d1c77612781cc50782
{dsjanainoyo} <{manouchekatsu@gmail.com}>
2023-02-07 01:16:56 +0900

    practice add 1file

---
+++ src/JSample2_1.java
@@ -0,0 +1,16 @@
+
+public class JSample2_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int sum = 0;
+		
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			sum += i;
+		}
+		
+		System.out.println("合計＝" + sum);
+	}
+
+}
